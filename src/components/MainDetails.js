d9fce8813427a73fc95f14b57a8cff8cd8cb023d
bwesterg <benton.westergaard@gmail.com>
2024-02-26 13:57:51 -0700

    adds basic structure, begins making form stateful.

---
+++ src/components/MainDetails.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function MainDetails() {
+    return (
+        <>
+            <section className="flex flex-col items-end justify-end">
+                {/* <input type="text" name="text" id="text" placeholder="Enter your name" required /> */}
+                <h2 className="text-xl uppercase">Benton</h2>
+                <p>Your Address</p>
+            </section>
+        </>
+    )
+}
---
+++ src/components/Notes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Notes() {
+    return (
+        <>
+            <section className="my-5">
+            {/* Textarea */}
+                <p>Notes to the client...</p>
+            </section>
+            {/* end of notes */}
+        </>
+    )
+}
